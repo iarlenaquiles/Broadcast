d857af68f439d1d5df007cbaa29c098f63264425
Iarlen <iarlensilva775@gmail.com>
2017-11-24 09:32:27 -0300

    Criando servidor Broadcast

--- src/br/ufc/quixada/sd/BroadcastServer.java
+++ src/br/ufc/quixada/sd/BroadcastServer.java
@@ -1,5 +1,31 @@
 package br.ufc.quixada.sd;
 
-public class BroadcastServer {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
+public class BroadcastServer {
+	public static void main(String[] args) {
+		DatagramSocket socket = null;
+
+		try {
+			socket = new DatagramSocket(5555, InetAddress.getByName("0.0.0.0"));
+			socket.setBroadcast(true);
+			while (true) {
+				byte[] receiveArray = new byte[1000];
+				DatagramPacket receivePacket = new DatagramPacket(receiveArray, receiveArray.length);
+				socket.receive(receivePacket);
+				String msg = new String(receivePacket.getData(), 0, receivePacket.getLength());
+				//System.out.println("Mensagem recebida:" + msg);
+				if(msg.equals("calc")){
+					//new Calculadora();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null)
+				socket.close();
+		}
+	}
 }
